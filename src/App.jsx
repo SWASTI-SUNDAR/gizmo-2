bc12db5c37223b0bdb27e148f11ab9bf7fe5b443
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2025-03-09 02:06:18 +0530

    feat: remove unused components and styles to streamline the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import "./App.css";
 import Navbar from "./components/PrimaryNav";
-// import EvaporationSimulation from "./pages/Evaporation";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Dissolving from "./pages/Dissolving";
 import Result from "./pages/Result";
 import ElectricalConductivityTester from "./pages/ElectricalConductivity";
-import Magnetisim from "./pages/Magnetisim";
 import MagneticPropertiesLab from "./pages/Testing";
 function App() {
   return (
@@ -19,7 +16,6 @@ function App() {
             path="/conductivity"
             element={<ElectricalConductivityTester />}
           />
-          <Route path="/magnetisi" element={<Magnetisim />} />
           <Route path="/magnetisim" element={<MagneticPropertiesLab />} />
           <Route path="/dissolving" element={<Dissolving />} />
           <Route path="/result" element={<Result />} />
